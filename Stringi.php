5c0ad578f9635665dffeb0b2c3fb00748833a583
targoszlukasz76-alt <targoszlukasz76@gmail.com>
2026-09-08 12:07:18 +0200

    08.09.2026

--- Stringi.php
+++ Stringi.php
@@ -2,4 +2,10 @@
     echo 'this is a simple string';
 
     echo 'You can also have embedded newlines in strings this way as it is okay to do';
+
+    //Output: Arnold once said: "I'll be back"
+    echo 'Arnold once said: "I\'ll be back"';
+
+    //Outputs: You deleted C:\*.*?
+    echo 'You deleted C:\\*.*?';
 ?>
