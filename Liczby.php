2d8b87c43008a40b0702df4574eed1c28eb9b480
targoszlukasz76-alt <targoszlukasz76@gmail.com>
2026-09-08 11:25:54 +0200

    08.09.2026

---
+++ Liczby.php
@@ -0,0 +1,20 @@
+/*
+    Skrypt PHP do demonstracji typów zmiennych
+/*
+<?php
+
+$liczba1 = 42;
+print("Liczba to: ".$liczba1);
+
+$LiczbaOctalna = 042;
+print("<br>Liczba osemkowa to: ".$LiczbaOctalna);
+
+$LiczbaHex = 0x2A;
+print("<br>Liczba szesnastkowa to: ".$LiczbaHex);
+
+$LiczbaBin = 0b101010;
+print("<br>Liczba binarna to: ".$LiczbaBin);
+
+$LiczbaDuza = 1_234_567;
+print("<br>Liczba duża to: ".$LiczbaDuza);
+?>
