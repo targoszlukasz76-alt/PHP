59d6f6d0431c236c494b94252112379eb6fe9d11
targoszlukasz76-alt <targoszlukasz76@gmail.com>
2026-09-08 11:36:04 +0200

    08.09.2026

--- Liczby.php
+++ Liczby.php
@@ -17,4 +17,10 @@ print("<br>Liczba binarna to: ".$LiczbaBin);
 
 $LiczbaDuza = 1_234_567;
 print("<br>Liczba duża to: ".$LiczbaDuza);
+
+$a = 1.234;
+$b = 1.2e3;     //1.2*10**3
+$c = 7E-10;     //7*10**(-10)
+$d = 1_234.567; //as of PHP 7.4.0
+
 ?>
