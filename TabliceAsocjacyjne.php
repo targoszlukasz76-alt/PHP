aef86d009295457b939c88b57533de671642d08b
targoszlukasz76-alt <targoszlukasz76@gmail.com>
2026-09-15 11:08:00 +0200

    15.09.2026

---
+++ TabliceAsocjacyjne.php
@@ -0,0 +1,10 @@
+<?php
+    $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+    $age2 = ["Peter"=>"35", "Ben"=>"37", "Joe"=>"43"];
+
+    $age3 = [];
+    $age3["Peter"] = "35";
+    $age3["Ben"] = "37";
+    $age3["Joe"] = "43";
+?>
