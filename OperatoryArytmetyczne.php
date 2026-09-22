9cde7c74fb4d5b4039fe141a9c9a29053bcac4b9
targoszlukasz76-alt <targoszlukasz76@gmail.com>
2026-09-22 10:10:07 +0200

    22.09.2026

---
+++ OperatoryArytmetyczne.php
@@ -0,0 +1,11 @@
+<?php
+    $a = 5;
+    $b = 3;
+    echo "Liczby a=$a, liczba b=$b <br>";
+    echo "suma a + b = ". ($a + $b)."<br>"; // 8
+    echo "różnica a - b = ". ($a - $b)."<br>"; // 2
+    echo "iloczyn a * b = ". ($a * $b)."<br>"; // 15
+    echo "iloraz a / b = ". ($a / $b)."<br>"; // 1.6666666666667
+    echo "reszta z dzielenia a % b = ". ($a % $b)."<br>"; // 2
+    echo "potęga a ** b = ". ($a ** $b)."<br>"; // 125
+?>
